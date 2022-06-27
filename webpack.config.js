f8738e19d400d64a10bee121d19fc7e1964bc76d
cwyyyds <1013953614@qq.com>
2022-06-27 20:55:00 +0800

    载入清除插件

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path')
 // 4.1引入自动生成 html 的插件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+// 5.1 引入清除插件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
   // 3.设置默认设置 自定义入口出口，安装有用插件
   entry: './src/index.js', //入口
@@ -16,5 +19,8 @@ module.exports = {
       template: './public/index.html', // 以哪个html文件为模板
       filename: 'index.html', // 生成的文件名称
     }),
+    // 5.0下载清除插件
+    new CleanWebpackPlugin(), // 删除的是ouput path 里配置的那个输出文件的文件夹
+    // 默认情况下dist
   ],
 }
